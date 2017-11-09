2aea25fc7fcd5b852b2a5f39204542102a57ff57
liel.k <liel.k@meuhedet.co.il>
2017-11-09 13:07:34 +0200

    add main upper navBar

--- src/app/points/components/points-side-bar/points-side-bar.component.ts
+++ src/app/points/components/points-side-bar/points-side-bar.component.ts
@@ -10,9 +10,107 @@ import { FADE_ANIMATION } from '../../../shared/animations/animations';
 
 export class PointsSideBarComponent implements OnInit {
 
+  particlesStyle: object = {};
+  particlesParams: object = {};
   constructor() { }
 
   ngOnInit() {
-  }
+    this.particlesStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '10vh',
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+      // 'z-index': -1    
+    };
 
-}
+    this.particlesParams = {
+          particles: {
+              number: {
+                  value: 1000,
+              },
+              color: {
+                  value: '#ffb87f'
+              },
+              shape: {
+                  type: 'circle'
+              },
+              opacity: {
+                value: 0.6,
+              },
+              size: {
+                'value': 1,
+                'random': true,
+                'anim': {
+                  'enable': false,
+                  'speed': 80,
+                  'size_min': 0.1,
+                  'sync': false
+                },
+              },
+              line_linked: {
+                'enable': false,
+                'distance': 0.1,          
+                'opacity': 0.3,
+              },
+              move: {
+                'enable': true,
+                'speed': 6,
+                'direction': 'none',
+                'random': false,
+                'straight': false,
+                'out_mode': 'out',
+                'bounce': false,
+                'attract': {
+                  'enable': true,
+                  'rotateX': 600,
+                  'rotateY': 1200
+                },
+              },
+            },
+            'interactivity': {
+              'detect_on': 'canvas',
+              'events': {
+                'onhover': {
+                  'enable': false,
+                  'mode': 'repulse'
+                },
+                'onclick': {
+                  'enable': false,
+                  'mode': 'push'
+                },
+                'resize': true
+              },
+              'modes': {
+                'grab': {
+                  'distance': 800,
+                  'line_linked': {
+                    'opacity': 1
+                  }
+                },
+                'bubble': {
+                  'distance': 800,
+                  'size': 80,
+                  'duration': 2,
+                  'opacity': 0.8,
+                  'speed': 3
+                },
+                'repulse': {
+                  'distance': 400,
+                  'duration': 0.4
+                },
+                'push': {
+                  'particles_nb': 4
+                },
+                'remove': {
+                  'particles_nb': 2
+                }
+              }
+            },            
+        }
+      };
+ }
+
+
--- src/app/points/points.module.ts
+++ src/app/points/points.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PointFormComponent } from '../points/components/point-form/point-form.component';
 import { PointsSideBarComponent } from '../points/components/points-side-bar/points-side-bar.component';
-
+import { SharedModule } from '../shared/shared.module';
+import { ParticlesModule } from 'angular-particle';
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     FormsModule,
+    ParticlesModule,
     ReactiveFormsModule
   ],
   providers: [PointService],
