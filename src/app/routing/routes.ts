d624abbe77aee07138f6dc8daeb3967344eafff4
liel.k <liel.k@meuhedet.co.il>
2017-11-12 18:03:42 +0200

    fml

--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router/router';
 import { PointFormComponent } from '../points/components/point-form/point-form.component';
 import { IntroComponent } from '../shared/components/intro/intro.component';
 import { BigLogoComponent } from '../shared/components/ui/big-logo/big-logo.component';
+import { MainViewComponent } from '../points/components/main-view/main-view.component';
 
 export const AppRoutes: Routes = [
     {
@@ -10,7 +11,7 @@ export const AppRoutes: Routes = [
     },
     {
       path: 'about',
-      component: PointFormComponent
+      component: MainViewComponent
     },
     {
       path: 'profile',
